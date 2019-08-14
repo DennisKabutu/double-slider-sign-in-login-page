668063551e1eff59c76879cf2f90e694df20b777
Dennis Kimani <denoh6804@gmail.com>
2019-08-14 16:29:03 +0300

    changed family font

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 //grab the sign up button,sign in button and container
 
-const signInButton = document.getElementById('signIn');
 const signUpButton = document.getElementById('signUp');
+const signInButton = document.getElementById('signIn');
 const container = document.getElementById('container');
 
 
-signInButton.addEventListener('click',()=>{
+signUpButton.addEventListener('click',() => {
     container.classList.add('right-panel-active');
 });
 
-signInButton.addEventListener('click',()=>{
+signInButton.addEventListener('click',()=> { 
     container.classList.remove('right-panel-active');    
 });
 
